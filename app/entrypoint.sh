5d631c0860fe39b716e0ce22d655933f93a3bb8b
Taishi Kasuga <kasuga@livesense.co.jp>
2020-07-11 20:12:23 +0900

    Add DEPLOY_USER var to env for capistrano and DRY yaml config

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -2,6 +2,11 @@
 
 set -eu
 
+mkdir -p /root/.ssh
+chmod 700 /root/.ssh
+echo ${GITHUB_PRIVATE_KEY} | sed -e 's#\\n#\n#g' > /root/.ssh/id_rsa
+chmod 600 /root/.ssh/id_rsa
+
 bundle exec rake railties:install:migrations
 bundle exec rake db:migrate || echo 'Skip migration'
 
